ba6e31bbe0a64da5e80e18443444426b5bcc1624
Scott Clark <scottclark591@gmail.com>
2015-02-09 17:33:19 -0500

    Fix typos in UsersController

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -2,7 +2,34 @@
 
 class UsersController extends BaseController {
 
-	public function create_temp() {
+	// create a new user
+	public function create_user() {
+		// create a new user
+		Log::info(Input::all());
+		User::create([
+			"email" => Input::get("email"),
+			"password" => Hash::make(Input::get("password"))
+		]);
+		// log that user in
+		Auth::attempt(array(
+			"email" => Input::get("email"),
+			"password" => Input::get("password")
+		));
+		// if they're logged in, add their new note and redirect to their all notes page
+		if(Auth::check()) {
+			$params = array(
+				"note"=>Input::get("note")
+			);
+			$request = Request::create('addnote', 'POST',$params);
+			Request::replace($request->input());
+			json_decode(Route::dispatch($request)->getContent());
+			return "success";
+		} else {
+			return "failed";
+		}
+	}
+
+	public function create_guest() {
 		Log::info(Input::all());
 		$start_email = "";
 		$email= $start_email.Hash::make(substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"),0,30))."@gmail.com";
@@ -63,32 +90,4 @@ class UsersController extends BaseController {
 			}
 		}
 	}
-
-	// create a new user
-	public function create_user() {
-		// create a new user
-		Log::info(Input::all());
-		User::create([
-			"email" => Input::get("email"),
-			"password" => Hash::make(Input::get("password"))
-		]);
-		// log that user in
-		Auth::attempt(array(
-			"email" => Input::get("email"),
-			"password" => Input::get("password")
-		));
-		// if they're logged in, add their new note and redirect to their all notes page
-		if(Auth::check()) {
-			$params = array(
-				"note"=>Input::get("note")
-			);
-			$request = Request::create('addnote', 'POST',$params);
-			Request::replace($request->input());
-			json_decode(Route::dispatch($request)->getContent());
-			return "success";
-		} else {
-			return "failed";
-		}
-	}
-
 }
