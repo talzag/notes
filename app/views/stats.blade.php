3b1e69a741f32a9fc94fa96046e14dab8054f4d6
Tommy Nicholas <tommy@knoxpayments.com>
2015-03-04 21:13:16 -0500

    Fixed error with not being logged in stats

--- app/views/stats.blade.php
+++ app/views/stats.blade.php
@@ -19,7 +19,7 @@
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <script src="js/vendor/modernizr-2.6.2.min.js"></script>
 </head>
-<body class="{{ "temp".Auth::user()->is_temporary }}">
+<body>
     <!--[if lt IE 7]>
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
