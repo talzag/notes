d56f129c01d937cda5e7f0a78f597b1ef1de21b4
Tommy Nicholas <tommy@knoxpayments.com>
2015-03-06 15:20:42 -0500

    Pushing just cuz

--- app/controllers/GoogleController.php
+++ app/controllers/GoogleController.php
@@ -2,27 +2,9 @@
 
 class GoogleController extends BaseController {
     // test Google API
+//     Response::json(array('success' => true, 'insert_id' => null), 200)
 	public function test() {
-        /************************************************
-          We'll setup an empty 1MB file to upload.
-         ************************************************/
-        DEFINE("TESTFILE", 'testupload.html');
-        if (!file_exists(TESTFILE)) {
-            $myfile = fopen(TESTFILE, "w") or die("Unable to open file!");
-            $head = "<html><head></head><body>";
-            $body = "<h1>Hello World</h1><p>HI!!!!</p>";
-            $footer = "</body></html>";
-            fwrite($myfile, $head);
-            fwrite($myfile, $body);
-            fwrite($myfile, $footer);
-            fclose($myfile);
-        }
-        
-        /************************************************
-          ATTENTION: Fill in these values! Make sure
-          the redirect URI is to this page, e.g:
-          http://localhost:8080/fileupload.php
-         ************************************************/
+    
         $client_id = '841513879584-fl6eokkpeut12lj2g328dmj64eqbc6s7.apps.googleusercontent.com';
         $client_secret = 'PqQPkpcCMycSyDjg7QT1I-GY';
         $redirect_uri = 'http://localhost/notes/public/google';
@@ -38,12 +20,14 @@ class GoogleController extends BaseController {
           Session::forget('upload_token');
         }
         
+/*
         if (isset($_GET['code'])) {
           $client->authenticate($_GET['code']);
           Session::put('upload_token', $client->getAccessToken());
           $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
           return Redirect::to($redirect_uri);
         }
+*/
         
         if (!empty(Session::get('upload_token')) && Session::get('upload_token')) {
           $client->setAccessToken(Session::get('upload_token'));
@@ -55,24 +39,33 @@ class GoogleController extends BaseController {
           return Redirect::to($authUrl);
         }
         
-        /************************************************
-          If we're signed in then lets try to upload our
-          file. For larger files, see fileupload.php.
-         ************************************************/
+        // If we have an accessToken and everything is good to go, build the file and upload it
         if ($client->getAccessToken()) {
+            // build a file that we're going to upload, wait shouldn't I do that later? 
+            DEFINE("TESTFILE", 'testupload.html');
+            if (!file_exists(TESTFILE)) {
+                $myfile = fopen(TESTFILE, "w") or die("Unable to open file!");
+                $head = "<html><head></head><body>";
+                $body = Input::get("note_text");
+                $footer = "</body></html>";
+                fwrite($myfile, $head);
+                fwrite($myfile, $body);
+                fwrite($myfile, $footer);
+                fclose($myfile);
+            }
                 
-          // Now lets try and send the metadata as well using multipart!
-          $file = new Google_Service_Drive_DriveFile();
-          $file->setTitle("Hello World!");
-          $result = $service->files->insert(
+            // Now lets try and send the metadata as well using multipart!
+            $file = new Google_Service_Drive_DriveFile();
+            $file->setTitle("Hello World!");
+            $result = $service->files->insert(
               $file,
               array(
                 'data' => file_get_contents(TESTFILE),
                 'convert' => true,
                 'uploadType' => 'multipart'
               )
-          );
-          unlink('testupload.html');
+            );
+            unlink('testupload.html');
         }
 	}
     public function viewtest() {
