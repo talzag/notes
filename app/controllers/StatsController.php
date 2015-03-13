adb25b0174b65600276f12611591aaccc622d6a8
Tommy Nicholas <tommy@knoxpayments.com>
2015-03-12 20:01:47 -0400

    Stats

--- app/controllers/StatsController.php
+++ app/controllers/StatsController.php
@@ -18,7 +18,7 @@ class StatsController extends BaseController {
     	    "new notes today" => Note::where( DB::raw('DAY(created_at)'), '=', $today )->count(),
     	    "new notes yesterday" => Note::where( DB::raw('DAY(created_at)'), '=', $yesterday) ->count(),
     	    "new gdocs today" => Gdoc::where( DB::raw('DAY(created_at)'), '=', $today )->count(),
-    	    "new gdocs today" => Gdoc::where( DB::raw('DAY(created_at)'), '=', $today )->count(),
+    	    "new gdocs yesterday" => Gdoc::where( DB::raw('DAY(created_at)'), '=', $yesterday )->count(),
     	    "new notes this month (".date('F').")" => Note::where( DB::raw('MONTH(created_at)'), '=', $thismonth )->count(),
     	    "new notes last month (".date('F',strtotime("-1 month")).")" => Note::where( DB::raw('MONTH(created_at)'), '=', $lastmonth )->count(),
     	    "current date" => date('d F Y G:i:a'),
