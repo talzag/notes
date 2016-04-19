8df4d5a17a31a353bdf637c91991bb708213cb5a
Tommy Nicholas <tomasienrbc@gmail.com>
2016-04-18 20:27:03 -0400

    Dumb seeder mistake

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -25,7 +25,7 @@ class UserTableSeeder extends Seeder {
 
   public function run() {
       // DB::table('users')->delete();
-      // User::create(array('email' => 'test@test.com'));
+      User::create(array('email' => 'test@test.com'));
   }
 
 }
