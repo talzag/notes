7fbf397ac4b1e797691c081bc20e49ab9319c3c1
Tommy Nicholas <tomasienrbc@gmail.com>
2016-05-01 14:42:21 -0400

    Fixing Database.php whoops

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'database'  => env('DB_DATABASE', getenv('DATABASE_NAME')),
+			'username'  => env('DB_USERNAME', getenv('DATABASE_USER')),
+			'password'  => env('DB_PASSWORD', getenv('DATABASE_PASSWORD')),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
